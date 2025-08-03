dfc6534b675cbf3c2dae70eae6d0b6d07b9605c8
shubhamhundare <shubhamhundare04@gmail.com>
2025-08-03 16:49:05 +0530

    Fixed the issue

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/datavolx">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/services" element={<Services />} />
